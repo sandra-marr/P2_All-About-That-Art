e008d984cf6ed5b749ab17d908ed47e01fef2b90
Rudy Sanchez <rudys212@gmail.com>
2021-06-29 22:05:39 -0700

    updated artist profile CSS

--- models/index.js
+++ models/index.js
@@ -1,12 +1,8 @@
-const Artwork = require('./Artwork');
-const BlogPost = require('./BlogPost');
-const Category = require('./Category');
-const Comment = require('./Commen');
-const Recommendation = require('./Recommendation');
-const User = require('./User');
-
-
-
-
+const Artwork = require("./Artwork");
+const BlogPost = require("./BlogPost");
+const Category = require("./Category");
+const Comment = require("./Comment");
+const Recommendation = require("./Recommendation");
+const User = require("./User");
 
 module.exports = { Artwork, BlogPost, Category, Comment, Recommendation, User };
