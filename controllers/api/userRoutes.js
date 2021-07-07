d1c1e05a5ca7c333796e0918851c48d380f23347
SPeats <srpeters44@gmail.com>
2021-07-07 09:23:40 -0600

    fixing signup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,28 +1,28 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const session = require('express-session');
 
-router.post('/', async (req, res) => {
-    try {
+//sign up
+router.post('/signup', async (req, res) => {
+  try {
+    
+    const userData = await User.create({
+     ...req.body
+    });
 
-      console.log("body",req.body);
-      const userData = await User.create({
-        ...req.body,
-        user_name: req.body.email
-      });
-      console.log("userData",userData);
-  
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-  
-        res.status(200).json(userData);
-      });
-    } catch (err) {
-      console.log("err",err);
-      res.status(400).json(err);
-    }
-  });
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
 
+      res.status(200).json(userData);
+    });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//login
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,7 @@
 const newArtHandler = async (event) => {
     event.preventDefault();
+
+    const name = document.querySelector('#name').value.trim();
   
     const description = document.querySelector('#description').value.trim();
     
@@ -10,7 +12,7 @@ const newArtHandler = async (event) => {
     if (description && category && image) {
       const response = await fetch(`/api/upload`, {
         method: 'POST',
-        body: JSON.stringify({ image , description, category}),
+        body: JSON.stringify({ name, description, category, image}),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -29,15 +31,21 @@ const newArtHandler = async (event) => {
   
 
 
-  const updateBio = async (event) => {
+const updateBio = async (event) => {
     event.preventDefault();
+
+    const str = window.location.pathname;
+    const id = str.split("/").reverse().join().split(",",1);
+
+    console.log(id);
+
     const newBio = document.querySelector('#text').value.trim();
     
   
     if (newBio) {
-      const response = await fetch(`/api/artist/updateBio`, {
+      const response = await fetch(`/api/dashboard/updateBio/${id}`, {
           method: 'PUT',
-          body: JSON.stringify({newBio}),
+          body: JSON.stringify({ newBio, id}),
           headers: { 'Content-Type': 'application/json' },
         });
         if (response.ok) {
